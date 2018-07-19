28f5524d50a9bbc235bd29d1c6dcfec241e85f7f
adsdkgit <adsdk.com@gmail.com>
2018-07-19 16:16:09 +0430

    fix

--- www/adsdk.js
+++ www/adsdk.js
@@ -1,38 +1,13 @@
 module.exports = {
-
-
-    //
-
-    init: function (developerId,isTestMode,isEnable)
-    {
-        var self = this;
+    initialize: function(developerId, test, enable) {
         cordova.exec(
-            function (result) {
-                console.log('setUp succeeded.' + result);
-
-                if (typeof result == "string") {
-
-
-                }
-                else {
-                    //if (result["event"] == "onXXX") {
-                    //	//result["message"]
-                    //	if (self.onXXX)
-                    //		self.onXXX(result);
-                    //}
-                }
-            },
-            function (error) {
-                console.log('setUp failed.');
-            },
-            'adsdkCordovaInterface',
+			function (result) {
+				console.log('Initialize Success.');
+			},
+			null,
+            'AdSdk',
             'init',
-            [developerId,isTestMode,isEnable]
-        );
+            [developerId, test, enable]
+        ); 
     }
-
-
-
-
-
 };
